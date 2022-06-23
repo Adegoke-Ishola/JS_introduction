da484adf6e052fd8154b84839c95fb8ea9f2e837
Adegoke <curtisadegoke@gmail.com>
2022-06-23 10:31:08 +0100

    my first commit

---
+++ me.js
@@ -0,0 +1,6 @@
+// Introduction to javaScript
+let nom = 'Adegoke';
+let age = 36;
+let height = 164.8;
+console.log('My name is '+ nom + ' and I am '+age+'years old.');
+console.log('I am also '+ height +'cm tall.');
