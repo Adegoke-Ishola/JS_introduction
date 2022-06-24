1a0c7e82198480aa471f21634ef1ed5ceabc5e1b
Adegoke <curtisadegoke@gmail.com>
2022-06-24 08:54:34 +0100

    height correction

--- me.js
+++ me.js
@@ -1,6 +1,6 @@
 // Introduction to javaScript
 let nom = 'Adegoke';
 let age = 36;
-let height = 164.8;
+let height = 164;
 console.log('My name is '+ nom + ' and I am '+age+'years old.');
-console.log('I am also '+ height +'cm tall.');
+console.log('I am also '+ height +'m tall.');
